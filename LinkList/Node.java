a27265c303d03464b041263bbf9f365db7e0079b
wangzhihao_pk <wangzh@pukkasoft.cn>
2022-05-27 10:05:59 +0800

    test

---
+++ LinkList/Node.java
@@ -0,0 +1,21 @@
+package com.pekopeko.LinkList;
+
+/**
+ * @author lustras
+ * @version 1.0
+ * @File Node.java
+ * @CreateDate 2022/5/279:20
+ * @Description <p>节点类型</p>
+ */
+public class Node {
+    // 数据域
+    private Object data;
+    // 指针域
+    private Node next;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+
+    
+}
