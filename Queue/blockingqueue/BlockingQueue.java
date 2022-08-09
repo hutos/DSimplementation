c129da139145347974462c7e605dc88e88d09b40
wangzhihao_pk <wangzh@pukkasoft.cn>
2022-08-09 16:52:14 +0800

    init

---
+++ Queue/blockingqueue/BlockingQueue.java
@@ -0,0 +1,11 @@
+package com.pekopeko.Queue.blockingqueue;
+
+/**
+ * @author lustras
+ * @version 1.0
+ * @File com.
+ * @CreateDate 2022/8/916:51
+ * @Description <p></p>
+ */
+public class BlockingQueue {
+}
